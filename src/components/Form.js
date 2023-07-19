05220afd1282f15d3f5ac3838af5279ff5493692
Nihal00 <nihallsuvarna@gmail.com>
2023-07-19 23:00:12 +0530

    submitting code-Wed Jul 19 2023 23:00:11 GMT+0530 (India Standard Time)

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Form = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-    const [user, setUser] = useState({
-        name: '',
-        email:'',
-        password: ''
-    })
-
-    function update(){
-        console.log(user);
-    }
-
+  function update() {
+    console.log(user);
+  }
 
   return (
     <>
       <form onSubmit={update}>
-        <label htmlFor='name'>Name</label>
-        <input type="text" id="name" onChange={(e) => setUser(...user, user.name)}/>
-        <label htmlFor='email'>Email</label>
-        <input type="email" id="email" onChange={(e) => setUser(...user, user.email)}/>
-        <label htmlFor='password'>Password</label>
-        <input type='password' id='password' onChange={(e) => setUser(...user, user.password)}/>
-        <button type='submit'>Submit</button>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={(e) => setUser(...user, user.name = e.target.value)}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={(e) => setUser(...user, user.email = e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setUser(...user, user.password = e.target.value)}
+        />
+        <button type="submit">Submit</button>
       </form>
     </>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
